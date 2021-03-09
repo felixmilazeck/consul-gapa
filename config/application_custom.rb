b00f6c46e62e3c683fc161c112b0e2787293f019
felix.milazeck <felix.milazeck@24creators.de>
2021-03-09 15:21:06 +0100

    locales

--- config/application_custom.rb
+++ config/application_custom.rb
@@ -1,5 +1,10 @@
 module Consul
   class Application < Rails::Application
     config.i18n.default_locale = :de
+
+    available_locales = [
+          "de",
+          "en",]
+        config.i18n.available_locales = available_locales
   end
 end
